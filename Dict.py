771301c0b9ff91da6611e24039d05d2e433ff879
jonathanwh <jonathanwh78@gmail.com>
2022-01-24 14:52:27 +0000

    commit

---
+++ Dict.py
@@ -0,0 +1,8 @@
+Games = {"Fifa":"EA", "COD":"Activision", "Assassins Creed":"Ubisoft"}
+
+print(Games["Fifa"])
+
+Games["God of War"] = "CD Prjeckt Red"
+
+print(Games)
+
---
+++ List.py
@@ -0,0 +1,9 @@
+games = ["Fifa", "COD", "Assassins Creed"]
+print(games[1])
+
+games.append("Persona 5")
+print(games)
+games.insert(0,"Just Cause 3")
+print(games)
+games.remove("COD")
+print(games)
