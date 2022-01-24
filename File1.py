65bf8a60e143a596bf940e45ec874049365c33da
jonathanwh <jonathanwh78@gmail.com>
2022-01-24 12:09:55 +0000

    Input based code

---
+++ File1.py
@@ -0,0 +1,14 @@
+firstname = "Jonathan"
+lastname = "White"
+print ("Hello " + firstname + " " + lastname)
+
+Firstname2 = input ("Please enter your First name: ")
+Lastname2 = input ("Please enter your Last name: ")
+
+print ("Your name is " + Firstname2 + " " + Lastname2)
+
+number1 = float(input("Please enter the first number: "))
+number2 = float(input("Please enter the second number: "))
+answer = number1 + number2
+
+print(number1, "+", number2, "=", answer)
